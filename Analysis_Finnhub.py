9c5ec2ca990e69d80292d97e3b6991787923ec0d
Ezzat Suhaime <ezzatisawesome@gmail.com>
2020-10-26 17:43:08 -0500

    Managed to get some API values connected

--- Analysis_Finnhub.py
+++ Analysis_Finnhub.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 from metrics import *
 import pandas as pd
 import numpy as np
@@ -7,7 +8,7 @@ import matplotlib.pyplot as plt
 
 with open("utilities_balance.csv", "r", newline = '') as csv_balance:
     balance_df = pd.read_csv(csv_balance)
-    balance_df.set_index([SYMBOL,YEAR], inplace=True)
+    balance_df.set_index([SYMBOL, PERIOD], inplace=True)
 
 with open("utilities_income.csv", "r", newline = '') as csv_income:
     income_df = pd.read_csv(csv_income)
@@ -17,20 +18,62 @@ with open("utilities_cashflows.csv", "r", newline = '') as csv_cashflows:
     cashflows_df = pd.read_csv(csv_cashflows)
     cashflows_df.set_index([SYMBOL, PERIOD], inplace=True)
 
+with open("utilities_estimates.csv", "r", newline = '') as csv_estimate:
+    estimates_df = pd.read_csv(csv_estimate)
+    estimates_df.set_index([SYMBOL], inplace=True)
+
+
+years = ['2020A', '2021B', '2022P', '2023P', '2024P', '2025P']
+sales = pd.Series(index = years, dtype='float64')
+
 
 projected_years = 5
-initial_sales = income_df.loc[('CPB','2020-08-02')][REVENUE]
-depr_percent = capex_percent = 0.032 #depr_percent: depreication rate / capex_percent: Capital Expenditure rate
-sales_growth_rate = 0.1 #sales_growth_rate: projected growth rate of sales for every future year
-ebitda_margin = 0.14 #ebitda margin: ebitda / total revenue
-nwc_percent = 0.24 #net working capital percent: (current asset - current liability) / sales of the year
-tax_rate = 0.25 #corporate income tax rate 
 
-#WACC & Terminal_Growth_Rate in the DCF Model
+#===== INITIAL SALES =====#
+initial_sales = sales['2020A'] = income_df.loc[('CPB','2020-08-02')][REVENUE] # DONE
+
+#===== DEPRECIATION RATE =====#
+depr_percent = capex_percent = 0.032 #depr_percent: depreication rate / capex_percent: Capital Expenditure rate
+
+#===== SALES GROWTH RATE =====#
+sales_growth_rate = 0 
+growth_rate_sum = 0
+for i in range(5):
+    try:
+        revenue1 = estimates_df.iloc[i][REVENUE_AVG]
+        revenue2 = estimates_df.iloc[i+1][REVENUE_AVG]
+        growth_rate_sum += (revenue1 / revenue2)
+    except:
+        print("Error - check revenue growth function")
+sales_growth_rate = growth_rate_sum / 5 # DONE
+        
+#===== EBITDA MARGIN =====#
+ebitda_margin = (income_df.loc[('CPB', '2020-08-02')][EBIT] + cashflows_df.loc[('CPB', '2020-08-02')][DEPRECIATION_AMORTIZATION]) / initial_sales # DONE
+
+#===== NET WORKING CAPITAL RATE =====#
+nwc_percent = (balance_df.loc[('CPB', '2020-08-02')][CURRENT_ASSETS] - balance_df.loc[('CPB', '2020-08-02')][CURRENT_LIABILITIES]) / initial_sales # DONE
+
+#===== CORPORATE INCOME TAX RATE =====#
+tax_rate = 0.25 #DONE
+
+#===== WACC & Terminal Growth Rate in the DCF Model =====#
 WACC = 0.12 #weighted average cost of capital
+
+total_debt = balance_df.loc[('CPB', '2020-08-02')][TOTAL_DEBT]
+total_equity = balance_df.loc[('CPB', '2020-08-02')][TOTAL_EQUITY]
+enterprise_value = total_equity + total_debt
+wd = total_debt / enterprise_value
+rd = .04 # arbitrary
+wp = 0
+rp = 0
+we = total_equity / enterprise_value
+de = .10 # need to use CAPM for this value
+
+WACC = wd * rd * (1-tax_rate) + wp * rp + we * de
+
 tgr = 0.02 #terminal growth rate of the company: for a mature company, the value maintains between 0.2-0.3%(long-term inflation rate) to 0.4-0.5%(History GDP growth rate)
 
-#Some parameters to create the Monte-Carlo Simulation
+#===== Monte-Carlo Simulation Parameters =====#
 iterations = 1000 #simulate 1000 times
 sales_std_dev = 0.01
 ebitda_std_dev = 0.02 
