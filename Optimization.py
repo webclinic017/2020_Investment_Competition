719206bde415283d14b96bd728d03c083a8748fe
Ezzat Suhaime <ezzatisawesome@gmail.com>
2020-11-14 12:10:30 -0600

    optimization finished

--- Optimization.py
+++ Optimization.py
@@ -1,29 +1,80 @@
 from helper_functions import get_eod_prices
+import Analysis_Finnhub as analysis
 import pandas as pd
+import csv
+import finnhub
 from pypfopt.expected_returns import mean_historical_return
 from pypfopt.risk_models import CovarianceShrinkage
 from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import CLA, plotting
 
 
-tickers = ['TSN', 'VZ', 'AMGN']
-output_file_path = "Output/prices.csv"
-token = "949f9442cbdff3e5ad488d93280c0b92ef042449"
+
+def optimize(sector, key, token, ticker_file_path, data_file_path):
+
+    #===== FINNHUB CLIENT =====#
+    finnhub_client = finnhub.Client(api_key=key2)
+
+    #===== ANALYSIS INSTANCE =====#
+    ana = analysis.Analysis_Finnhub(finnhub_client, "Data/{}_balance_annual.csv".format(sector), 
+                                                "Data/{}_income_ttm.csv".format(sector), 
+                                                "Data/{}_income_annual.csv".format(sector),
+                                                "Data/{}_cashflows_ttm.csv".format(sector), 
+                                                "Data/{}_cashflows_annual.csv".format(sector), 
+                                                "Data/{}_basic_financials.csv".format(sector),
+                                                "Data/{}_estimates.csv".format(sector))
+
+    #===== OBTAIN DATA =====#
+    return_data = {}
+
+    with open(ticker_file_path, "r", newline = '') as ticker_sheet:
+        ticker_array = [] # For use in get_eod_prices function
+        
+        tickers = csv.reader(ticker_sheet, delimiter=' ', quotechar='|')
+        
+        for row in tickers:
+            ticker = row[0]
+            ticker_array.append(ticker)
+            
+            data_array = ana.buffett(ticker)
+
+            projected_growth_rate = (data_array["pe_min"] + data_array["pe_max"]) / 2
+
+            return_data[ticker] = projected_growth_rate / 100
+
+        #get_eod_prices(ticker_array, data_file_path, token)
+
+    #===== OPTIMIZATION =====#
+    with open(data_file_path, "r", newline = '') as price_sheet:
+        # Get historical prices
+        prices_df = pd.read_csv(price_sheet)
+        prices_df.set_index(["date"], inplace=True)
+        
+        # Get returns
+        mu = pd.Series(return_data)
+        #mu = mean_historical_return(prices_df)
+
+        # Get risk
+        S = CovarianceShrinkage(prices_df).ledoit_wolf()
+        # Optimize
+        ef = EfficientFrontier(mu, S)
+        ef.max_sharpe()
+        cleaned_weights = ef.clean_weights()
+
+        # Print out to terminal
+        ef.portfolio_performance(verbose=True)
+
+        ef.save_weights_to_file("weights.txt")
 
 
-get_eod_prices(tickers, output_file_path, token)
+if __name__ == "__main__":
+    #===== INPUT PARAMETERS =====#
+    key1 = 'bucae7f48v6oa2u4ng20'
+    key2 = 'btts0j748v6ojt2hie60'
+    token = "949f9442cbdff3e5ad488d93280c0b92ef042449"
 
-with open(output_file_path, "r", newline = '') as price_sheet:
-    prices_df = pd.read_csv(price_sheet)
-    prices_df.set_index(['date'], inplace=True)
+    sector = 'portfolio'
+    ticker_file_path = "Tickers/{}.csv".format(sector)
+    data_file_path = "Data/{}_prices.csv".format(sector)
 
-    mu = mean_historical_return(prices_df)
-    S = CovarianceShrinkage(prices_df).ledoit_wolf()
-
-    ef = EfficientFrontier(mu, S)
-    weights = ef.max_sharpe()
-    cleaned_weights = ef.clean_weights()
-    ef.save_weights_to_file("weights.txt")  # saves to file
-
-    print(cleaned_weights)
-
-    ef.portfolio_performance(verbose=True)
+    optimize(sector, key1, token, ticker_file_path, data_file_path)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import Analysis_Finnhub as analysis
-import helper_functions as hf
 import csv
 import math
 import matplotlib.pyplot as plt
@@ -7,97 +6,103 @@ import finnhub
 from docx import Document
 from docx.shared import Inches
 
-#===== INPUT PARAMETERS =====#
-sector = 'h'
-key1 = 'bucae7f48v6oa2u4ng20'
-key2 = 'btts0j748v6ojt2hie60'
 
-buffett_url = "/Users/ezzatsuhaime/Desktop/dcf_{}.csv".format(sector)
-dcf_url = "/Users/ezzatsuhaime/Desktop/dcf_{}.docx".format(sector)
+def main(sector, key, output_url):
 
+    #===== FINNHUB CLIENT =====#
+    finnhub_client = finnhub.Client(api_key=key2)
 
-#===== FINNHUB CLIENT =====#
-finnhub_client = finnhub.Client(api_key=key2)
+    #===== US COMPANIES =====#
+    us_tickers = "Tickers/{}.csv".format(sector) # file path name
 
-#===== US COMPANIES =====#
-us_tickers = "Tickers/{}.csv".format(sector) # file path name
+    #===== FILES =====#
+    document = Document()
+    document.add_heading('Monte-Carlo Discounted Cash Flow Model and Buffett Analysis', 0)
 
-#===== FILES =====#
-document = Document()
-document.add_heading('Monte-Carlo Discounted Cash Flow Model and Buffett Analysis', 0)
-
-#===== ANALYSIS CLASS =====#
-ana = analysis.Analysis_Finnhub(finnhub_client, "Data/{}_balance_annual.csv".format(sector), 
-                                        "Data/{}_income_ttm.csv".format(sector), 
-                                        "Data/{}_income_annual.csv".format(sector),
-                                        "Data/{}_cashflows_ttm.csv".format(sector), 
-                                        "Data/{}_cashflows_annual.csv".format(sector), 
-                                        "Data/{}_basic_financials.csv".format(sector),
-                                        "Data/{}_estimates.csv".format(sector))
+    #===== ANALYSIS CLASS =====#
+    ana = analysis.Analysis_Finnhub(finnhub_client, "Data/{}_balance_annual.csv".format(sector), 
+                                            "Data/{}_income_ttm.csv".format(sector), 
+                                            "Data/{}_income_annual.csv".format(sector),
+                                            "Data/{}_cashflows_ttm.csv".format(sector), 
+                                            "Data/{}_cashflows_annual.csv".format(sector), 
+                                            "Data/{}_basic_financials.csv".format(sector),
+                                            "Data/{}_estimates.csv".format(sector))
 
 
-#=============== ANALYSIS ===============#
-with open(us_tickers, "r", newline = '') as ticker_file:
-    ticker_reader = csv.reader(ticker_file, delimiter=' ', quotechar='|')
-    
-    for row in ticker_reader:   
-        ticker = row[0] # ticker
+    #=============== ANALYSIS ===============#
+    with open(us_tickers, "r", newline = '') as ticker_file:
+        ticker_reader = csv.reader(ticker_file, delimiter=' ', quotechar='|')
         
-        data_array = ana.buffett(ticker) # get Buffett analysis
-        
-        if(data_array is not None):
+        for row in ticker_reader:   
+            ticker = row[0] # ticker
             
-            if(data_array["p_agr_min"] > 0): # if min projected annual growth is positive
-                document.add_heading("TICKER: {}".format(ticker), level=1)
+            data_array = ana.buffett(ticker) # get Buffett analysis
+            
+            if(data_array is not None):
                 
-                try:
-                    image_path = "Images/{}.png".format(ticker)
-                    image_path_2 = "Images/projection_{}.png".format(ticker)
+                if(data_array["p_agr_min"] > 0): # if min projected annual growth is positive
+                    document.add_heading("TICKER: {}".format(ticker), level=1)
                     
-                    # Monte Carlo Analysis
-                    plt.hist(ana.monte_carlo_DCF(ticker), bins = 30, density = True, color = "r")
-                    plt.savefig(image_path)
-                    document.add_picture(image_path, width=Inches(3.00))
-                    plt.clf()
+                    try:
+                        image_path = "Images/{}.png".format(ticker)
+                        image_path_2 = "Images/projection_{}.png".format(ticker)
+                        
+                        # Monte Carlo Analysis
+                        plt.hist(ana.monte_carlo_DCF(ticker), bins = 30, density = True, color = "r")
+                        plt.savefig(image_path)
+                        document.add_picture(image_path, width=Inches(3.00))
+                        plt.clf()
 
-                    # Values for chart
-                    length = data_array["length"]
-                    eps_array = data_array['eps_array']
-                    cagr_eps = data_array["cagr_eps"]
-                    eps1 = data_array["eps1"]
-                    projected_eps =  eps1 * math.pow(1+cagr_eps, 10)
-                    coef = data_array["coef"][0]
-                    intercept =  data_array["intercept"]
-                    final_y = intercept + length * coef
-                    
-                    
-                    # Drawing lines for chart
-                    plt.scatter(data_array['count_array'], eps_array)
-                    plt.plot([0, length], [intercept, final_y])
-                    plt.plot([length, length+10], [final_y, projected_eps])
+                        # Values for chart
+                        length = data_array["length"]
+                        
+                        eps1 = data_array["eps1"]
+                        eps_array = data_array['eps_array']
+                        cagr_eps = data_array["cagr_eps"]
+                        projected_eps =  eps1 * math.pow(1+cagr_eps, 10)
+                        
+                        coef = data_array["coef"][0]
+                        intercept =  data_array["intercept"]
+                        final_y = intercept + length * coef
+                        
+                        
+                        # Drawing lines for chart
+                        plt.scatter(data_array['count_array'], eps_array)
+                        plt.plot([0, length], [intercept, final_y])
+                        plt.plot([length, length+10], [final_y, projected_eps])
 
-                    # Save chart
-                    plt.savefig(image_path_2)
-                    document.add_picture(image_path_2, width=Inches(3.0))
-                    plt.clf()
+                        # Save chart
+                        plt.savefig(image_path_2)
+                        document.add_picture(image_path_2, width=Inches(3.0))
+                        plt.clf()
 
-                    # Write data describing chart
-                    paragraph = document.add_paragraph("R-Value ROE: {}".format(data_array['r_value_roe']))
-                    paragraph = document.add_paragraph("R-Value EPS: {}".format(data_array['r_value_eps']))
-                    paragraph = document.add_paragraph("CAGR EPS: {}".format(data_array['cagr_eps']))
-                    paragraph = document.add_paragraph("PE Min: {}".format(data_array['pe_min']))
-                    paragraph = document.add_paragraph("PE Max: {}".format(data_array['pe_max']))
-                    paragraph = document.add_paragraph("Projected AGR Min: {}".format(data_array['p_agr_min']))
-                    paragraph = document.add_paragraph("Projected AGR Max: {}".format(data_array['p_agr_max']))
+                        # Write data describing chart
+                        paragraph = document.add_paragraph("R-Value ROE: {}".format(data_array['r_value_roe']))
+                        paragraph = document.add_paragraph("R-Value EPS: {}".format(data_array['r_value_eps']))
+                        paragraph = document.add_paragraph("CAGR EPS: {}".format(data_array['cagr_eps']))
+                        paragraph = document.add_paragraph("PE Min: {}".format(data_array['pe_min']))
+                        paragraph = document.add_paragraph("PE Max: {}".format(data_array['pe_max']))
+                        paragraph = document.add_paragraph("Projected AGR Min: {}".format(data_array['p_agr_min']))
+                        paragraph = document.add_paragraph("Projected AGR Max: {}".format(data_array['p_agr_max']))
 
-                    document.add_page_break()
-                    #document.add_paragraph("\n\n")
+                        document.add_page_break()
+                        #document.add_paragraph("\n\n")
 
-                except Exception as e:
-                    print(ticker, " : Error - ", e)
-                    #print(ticker"Something happened with", ticker)
-                    document.add_paragraph("Something wrong happened")
-                    plt.clf()
+                    except Exception as e:
+                        print(ticker, " : Error - ", e)
+                        #print(ticker"Something happened with", ticker)
+                        document.add_paragraph("Something wrong happened")
+                        plt.clf()
 
-document.save(dcf_url)
-ticker_file.close()
+    document.save(output_url)
+    ticker_file.close()
+
+
+if __name__ == "__main__":
+    #===== INPUT PARAMETERS =====#
+    sector = 'portfolio'
+    key1 = 'bucae7f48v6oa2u4ng20'
+    key2 = 'btts0j748v6ojt2hie60'
+    url = "/Users/ezzatsuhaime/Desktop/dcf_{}.docx".format(sector)
+
+    main(sector, key1, url)
